9ef8bf1f69707bac6923bc678600b9dfabb5dbd0
pablomedina7 <pablomedinasantos@hotmail.com>
2024-08-26 10:18:54 -0400

    Eliminar import innecesario de sys

--- ACCUWHEATER/weather_report.py
+++ ACCUWHEATER/weather_report.py
@@ -1,10 +1,15 @@
 import requests
-import sys
 import argparse
 import json
 import csv
+from dotenv import load_dotenv
+import os
 
-API_KEY = '42a2c636573464e6bff2055c028c2cf8'
+# Cargar variables de entorno desde .env
+load_dotenv()
+
+# Obtener API_KEY del archivo .env
+API_KEY = os.getenv('API_KEY')
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 
 def get_weather(city, format_output):
